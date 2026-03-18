aae28185174eca8c070ec50cebf6131e87a9b1fe
correo11011correo-netizen <correo11011correo@gmail.com>
2026-03-18 11:58:41 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Archivo de configuración para tu proyecto
+
+const CONFIG = {
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwDSE3LT_pmfC3JcMYKYjVIxeQPkGgluwWRK1aNHBCRkrBGc7aSv3w0PK-JlyhwZVUb/exec",
+  CLIENT_ID: "527108731574-nvkf2sovotbn1vtq7fnh42o6ujkt9lj7.apps.googleusercontent.com",
+  FOLDER_ID: "ID_DE_TU_CARPETA_EN_DRIVE" // opcional, si lo usás en Apps Script
+};
