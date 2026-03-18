f57c52ae71c168ba73ff72673aed83ac589ce705
correo11011correo-netizen <correo11011correo@gmail.com>
2026-03-18 12:11:51 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Archivo de configuración para tu proyecto
 
 const CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwbBLjLjUPa1nzFokinsTCaBvtKnre29pqI9bsiTGdocFMYT7ljzhP3sGYX6-y-kpBP/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw7hqxmLeKcroLJMNsAmU1eIJQICX4EF8ADcLybvKH8_Phj6sTFyH_9q2KHJV1glOcw/exec",
   CLIENT_ID: "527108731574-nvkf2sovotbn1vtq7fnh42o6ujkt9lj7.apps.googleusercontent.com",
-  FOLDER_ID: "1HuZ3XmstWNbF90bwEWxkwup_kyxGYyg_" // <-- tu carpeta real en Drive
+  FOLDER_ID: "1HuZ3XmstWNbF90bwEWxkwup_kyxGYyg_" // tu carpeta real en Drive
 };
