07954ed0f76b609c5c16403372309ca57a7d6863
correo11011correo-netizen <correo11011correo@gmail.com>
2026-03-18 12:53:52 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycby2jK6lTYlvjf0vFqFsQY65E5CM1kcH07Scg2vUHoGmuOWn4uwDK_6sBIjCV79Ism2r/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz_dVuCZ25U7LVJcyqpCRYzwPY32zblTEoP0MTaPAQZoTQcRp2wUQNrm5mnsjWLFpoO/exec",
   CLIENT_ID: "527108731574-nvkf2sovotbn1vtq7fnh42o6ujkt9lj7.apps.googleusercontent.com",
   FOLDER_ID: "1HuZ3XmstWNbF90bwEWxkwup_kyxGYyg_"
 };
